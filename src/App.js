ad7ff914c7d6c73c1e3fc215a9fd53b6021a10e1
Nahum <losond8@gmail.com>
2021-03-19 16:14:55 -0400

    Lift the state from the child to the parent, and pass the state info back to the child via props, implemented a click event called to the UI and rendered details using a card component from reactstrap - Components Part 1 & 2 Completed.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 //import logo from './logo.svg';
+//import 'semantic/dist/semantic.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './components/shared/dishes';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="success">
-        <div className="container">
-          <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand> 
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+//Lifting the state of the dishes from {Menu} and imported the {DISHES} above to be made available below and other components. 
+
+//In order to store the state we need to transform the functional component into a class component.
+
+class App extends Component {
+  constructor(props){
+    super(props);
+//dishes defined in the state is now avaiable as props to my menu component through props
+      this.state = {
+        dishes: DISHES
+      };
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Navbar dark color="success">
+          <div className="container">
+            <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand> 
+          </div>
+        </Navbar>
+{/* <!-- pass my state as props into the {Menu} --> */}
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,72 +1,65 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 import { Media } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-//stores properties that I want to use in this component, which is an arr of objs 
+//stores properties that I want to use in this component, which is an arr of objs (R.C part 1)
         this.state ={
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ],
+//clear out the state of dishes because it is availabe for use as props from the {App} (R.C part 2)
+//Add a new state to keep track of when a selected is has been triggered from null to selected
+            selectedDish: null
+        }
+    }
+
+//handler function to handle my event to updated the state a selectedDish from null to displaying content on the DOM by using this.setState
+    onDishSelect(dish){
+        this.setState({selectedDish: dish})
+    }
+
+    renderDish(dish) {
+        if(dish != null){
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        }
+        else {
+            return <div>Nothing to see here!</div>
         }
     }
 
     render() {
 //Defining a variable called menu that will map over the dishes arr and for each item it will layout each and every dish item. 
-        const menu = this.state.dishes.map(dish => {
+        const menu = this.props.dishes.map(dish => {
             return (
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+{/* Add e.listener, pass in the handler that will be responsible for what happens when this event is triggered. */}
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+
                 </div>
             );
         });
 // Use the js variable menu to return my constructed media list of items
+//To display the selectedDish content, pass in the renderDish method and pass in the state as a param, so when the method is called it will return the dish in the form of a card that will be displayed beneath the list of cards.
         return (
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         )
